b4fdf3f12c81d7a58b359430d30d66ecca496dc4
Shayna Levin <shayna.levin@cooper.edu>
2025-04-24 10:53:24 -0400

    Fixed repeating cards bug

--- deck.py
+++ deck.py
@@ -2,7 +2,7 @@ import random
 from card import Card
 
 class Deck:
-    SUITS = ['H', 'D', 'C', 'S']          # Hearts, Diamonds, Clubs, Spades
+    SUITS = ['H', 'D', 'C', 'S']
     RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
     def __init__(self, include_jokers: bool = False):
@@ -10,9 +10,7 @@ class Deck:
         self._build()
 
     def _build(self):
-        self.cards = [Card(rank, suit) 
-                      for suit in self.SUITS 
-                      for rank in self.RANKS]
+        self.cards = [Card(rank, suit) for suit in self.SUITS for rank in self.RANKS]
         if self.include_jokers:
             self.cards += [Card(None, None, is_joker=True) for _ in range(2)]
 
