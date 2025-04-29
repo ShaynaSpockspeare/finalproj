619ac203e6cd53df8ba07cabadf8cb510a84b1c0
Shayna Levin <shayna.levin@cooper.edu>
2025-04-28 22:02:01 -0400

    Added comments. Speaking of which can you plz put comments for deck.py tysm

--- player.py
+++ player.py
@@ -3,21 +3,26 @@ class Player:
         self.name = name
         self.hand = []
         self.score = 0
-        self.wins_in_a_row = 0  # Track win streaks
+        self.wins_in_a_row = 0 
 
     def draw_card(self, cards):
+        # This function adds cards to the player's hand after drawing
         self.hand.extend(cards)
 
     def remove_cards(self, cards):
+        # This function removes cards from the player's hand when placed
         for c in cards:
             self.hand.remove(c)
 
     def hand_value(self):
+        # This function calculates the total value of the player's hand
         return sum(card.value() for card in self.hand)
 
     def reset_hand(self):
+        # The hand has to reset between rounds
         self.hand = []
 
     def __repr__(self):
+        # This function returns a string representation of the player's hand just for simplicity, may remove later
         return f"{self.name}'s hand: {', '.join(map(str, self.hand))}"
 
